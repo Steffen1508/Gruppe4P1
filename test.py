94ea6b4ce0d450b07c9390facb1526bd117f1fd5
021021-dani <danielparhizkari@gmail.com>
2026-02-27 15:50:20 +0100

    Tilføjet grundlæggende data-inspektion

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import pandas as pd
+
 df = pd.read_parquet("hf://datasets/syvai/pii-dataset-eng/data/train-00000-of-00001.parquet")
 print(df.head())
+print("Antal rækker:",len(df))
+print("Antal klonner:", df.shape[1])
+print(df.isnull().sum())   # --> Show Missing Value 
 df.info()
 
